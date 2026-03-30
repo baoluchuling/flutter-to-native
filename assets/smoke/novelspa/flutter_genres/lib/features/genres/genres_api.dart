f05bbbfd491ccc44f3f7720bafd6bbecf2c78f34
baoluchuling <baoluchuling@126.com>
2026-03-30 20:44:15 +0800

    Remove unused assets/smoke test fixtures

--- assets/smoke/novelspa/flutter_genres/lib/features/genres/genres_api.dart
+++
@@ -1,9 +0,0 @@
-class GenresApi {
-  Future<void> getGenres() async {
-    await dio.get("/genres/list");
-  }
-
-  Future<void> fetchGenreBooks() async {
-    await dio.get("/genres/books");
-  }
-}
--- assets/smoke/novelspa/flutter_genres/test/genres_filter_page_test.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-  testWidgets("loads genres filter page", (tester) async {});
-  testWidgets("retries after error", (tester) async {});
-  testWidgets("loads more books when tapping button", (tester) async {});
-}
